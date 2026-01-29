6da8b3bde67f477a966fff05a7f9e0cf6cfeae53
Veronika Mohr <veronika.mohr@hereon.de>
2026-01-29 10:28:55 +0100

    corrected links in merge_hotstart.sh

--- merge_hotstart.sh
+++ merge_hotstart.sh
@@ -18,7 +18,7 @@ export I_MPI_PMI_LIBRARY=/use/lib64/libmpi.so
 export LD_LIBRARY_PATH="/sw/spack-levante/mambaforge-22.9.0-2-Linux-x86_64-kptncg/lib:$LD_LIBRARY_PATH"
 
 #read arguments
-ver=$1
+ver=$1 #SGINT step; starting from 1 after 
 
 #define important directories
 this_dir=$PWD
@@ -54,17 +54,17 @@ cp outputs/mirror.out $new
 echo "Mirror file copied to $new"
 
 #calculate means for sgint 
-sgintmeanpath=/home/g/g260204/schism_v5.11/sgint_scripts/make_week_mean_parallel.py
+sgintmeanpath=/home/g/g260204/schism_v5.11/sgint_scripts_clean/make_week_mean_parallel.py
 python3 $sgintmeanpath $this_dir $ver
 echo "Week mean calculated for $ver"
 
 #calculate sg int
-sgintpath=/home/g/g260204/schism_v5.11/sgint_scripts/run_sgint_parallel.py
+sgintpath=/home/g/g260204/schism_v5.11/sgint_scripts_clean/run_sgint_parallel.py
 python3 $sgintpath $this_dir $ver
 echo "SG int calculated for $ver"
 
 #calculate POC source
-makesourcepath=/home/g/g260204/schism_v5.11/sgint_scripts/create_source/create_source_nc_sep.py
+makesourcepath=/home/g/g260204/schism_v5.11/sgint_scripts_clean/create_source/create_source_nc_sep.py
 python3 $makesourcepath $this_dir $ver
 rm $this_dir\/source.nc
 ln -s $this_dir\/outputs/week_mean/source_$ver.nc $this_dir\/source.nc
@@ -72,6 +72,6 @@ echo "source file created for $ver"
 
 #calculate POC sediment changes
 cp $this_dir\/outputs\/hotstart_it\=$biggest_t\.nc $this_dir\/outputs\/hotstart_it\=$biggest_t\_orig.nc
-updatehotpath=/home/g/g260204/schism_v5.11/sgint_scripts/create_sed_source/sed_source_sep.py
+updatehotpath=/home/g/g260204/schism_v5.11/sgint_scripts_clean/create_sed_source/sed_source_sep.py
 python3 $updatehotpath $this_dir $ver
 echo "updated sediment fraction in hotstart"
